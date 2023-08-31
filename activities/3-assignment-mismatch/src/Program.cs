0cee34650802ccbd0d340df2555490fc38d01598
Sam Attwood <59017719+samattwood9@users.noreply.github.com>
2023-08-31 10:24:06 +0000

    Add src for assignment 3

---
+++ activities/3-assignment-mismatch/src/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Declaration
+        int myInt;
+        double myDouble;
+        decimal myDecimal;
+        string myString;
+        bool myBool;
+
+        // Assignment
+        myInt = 27;
+        myDouble = 27.53;
+        myDecimal = 23.0M;
+        myString = "This is a string.";
+        myBool = false;
+
+        // Assignment (again)
+        // NOTE: These are all mismatched and will errors! Try entering dotnet run into the terminal to see them for yourself.
+        // TODO: Have a go at fixing these assignment statements by assigning the new values to the appropriate variables.
+        myInt = "This is another string.";
+        myDouble = 10.1M;
+        myDecimal = true;
+        myString = 100.123;
+        myBool = 99;
+        
+        // Output to terminal
+        Console.WriteLine(myInt);
+        Console.WriteLine(myDouble);
+        Console.WriteLine(myDecimal);
+        Console.WriteLine(myString);
+        Console.WriteLine(myBool);
+    }
+}
